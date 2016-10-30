9a434ce8326a5c363ccabb07b989dc954fcbc50c
sxj1996619 <sxj1996619@gmail.com>
2016-10-30 23:30:31 +0800

    immense change

--- maze.js
+++ maze.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var canvas;
 var ctx;
 var dx = 5;
@@ -101,7 +100,7 @@ function draw() {
 }
 init();
 window.addEventListener('keydown', doKeyDown, true);
-=======
+
 
 var canvas;
 var ctx;
@@ -114,91 +113,94 @@ var HEIGHT = 482;
 var img = new Image();
 var collision = 0;
 
-function rect(x,y,w,h) {
-ctx.beginPath();
-ctx.rect(x,y,w,h);
-ctx.closePath();
-ctx.fill();
+function rect(x, y, w, h) {
+    ctx.beginPath();
+    ctx.rect(x, y, w, h);
+    ctx.closePath();
+    ctx.fill();
 }
 
 function clear() {
-ctx.clearRect(0, 0, WIDTH, HEIGHT);
-ctx.drawImage(img, 0, 0);
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    ctx.drawImage(img, 0, 0);
 }
 
 function init() {
-canvas = document.getElementById("canvas");
-ctx = canvas.getContext("2d");
-img.src = "maze.gif";
-return setInterval(draw, 10);
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    img.src = "maze.gif";
+    return setInterval(draw, 10);
 }
 
-function doKeyDown(evt){
-switch (evt.keyCode) {
-case 38:  /* Up arrow was pressed */
-if (y - dy > 0){
-y -= dy;
-clear();
-checkcollision();
-if (collision == 1){
-y += dy;
-collision = 0;
-}
-}
+function doKeyDown(evt) {
+    switch (evt.keyCode) {
+        case 38:
+            /* Up arrow was pressed */
+            if (y - dy > 0) {
+                y -= dy;
+                clear();
+                checkcollision();
+                if (collision == 1) {
+                    y += dy;
+                    collision = 0;
+                }
+            }
 
-break;
-case 40:  /* Down arrow was pressed */
-if (y + dy < HEIGHT ){
-y += dy;
-clear();
-checkcollision();
-if (collision == 1){
-y -= dy;
-collision = 0;
-}
-}
+            break;
+        case 40:
+            /* Down arrow was pressed */
+            if (y + dy < HEIGHT) {
+                y += dy;
+                clear();
+                checkcollision();
+                if (collision == 1) {
+                    y -= dy;
+                    collision = 0;
+                }
+            }
 
-break;
-case 37:  /* Left arrow was pressed */
-if (x - dx > 0){
-x -= dx;
-clear();
-checkcollision();
-if (collision == 1){
-x += dx;
-collision = 0;
-}
-}
-break;
-case 39:  /* Right arrow was pressed */
-if ((x + dx < WIDTH)){
-x += dx;
-clear();
-checkcollision();
-if (collision == 1){
-x -= dx;
-collision = 0;
-}
-}
-break;
-}
+            break;
+        case 37:
+            /* Left arrow was pressed */
+            if (x - dx > 0) {
+                x -= dx;
+                clear();
+                checkcollision();
+                if (collision == 1) {
+                    x += dx;
+                    collision = 0;
+                }
+            }
+            break;
+        case 39:
+            /* Right arrow was pressed */
+            if ((x + dx < WIDTH)) {
+                x += dx;
+                clear();
+                checkcollision();
+                if (collision == 1) {
+                    x -= dx;
+                    collision = 0;
+                }
+            }
+            break;
+    }
 }
 
 function checkcollision() {
-var imgd = ctx.getImageData(x, y, 15, 15);
-var pix = imgd.data;
-for (var i = 0; n = pix.length, i < n; i += 4) {
-if (pix[i] == 0) {
-collision = 1;
-}
-}
+    var imgd = ctx.getImageData(x, y, 15, 15);
+    var pix = imgd.data;
+    for (var i = 0; n = pix.length, i < n; i += 4) {
+        if (pix[i] == 0) {
+            collision = 1;
+        }
+    }
 }
 
 function draw() {
-clear();
-ctx.fillStyle = "purple";
-rect(x, y, 15,15);
+    clear();
+    ctx.fillStyle = "purple";
+    rect(x, y, 15, 15);
 }
 init();
-window.addEventListener('keydown',doKeyDown,true);
->>>>>>> origin/master
+window.addEventListener('keydown', doKeyDown, true);
