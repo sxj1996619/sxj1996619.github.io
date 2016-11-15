458f91efc6a5ea1bf4c06da3272978e1d69e4151
sxj1996619 <sxj1996619@gmail.com>
2016-11-15 15:56:29 +0800

    Add files via upload

--- js/myd3.js
+++ js/myd3.js
@@ -1,5 +1,9 @@
-function Run() {
+var x1 = 100;
+var y1 = 299;
 
+function Run() {
+    $('rect').remove();
+    $('circle').remove();
     console.log('run...');
     var svgctrl = d3.select('svg');
 
@@ -7,39 +11,42 @@ function Run() {
 
     c1.attr('x', 50).attr('y', 20).attr('width', "154px").attr('height', "310px").attr('style', "fill:#ededed").attr('stroke-width', '5px').append('id', "img1");
 
-    $('#img1').bind('click', function() {
-        $('#img1').animate({ left: '20px' }, "slow");
-    })
-
-
     var c2 = svgctrl.append('rect');
 
-    c2.attr('x', 57).attr('y', 40).attr('width', "140px").attr('height', "240px").attr('style', "fill:black").attr('stroke-width', '5px');
+    c2.attr('x', 57).attr('y', 40).attr('width', "140px").attr('height', "240px").attr('style', "fill:black").attr('stroke-width', '5px').append('id', "img2");
 
     var c3 = svgctrl.append('rect');
 
-    c3.attr('x', 90).attr('y', 299).attr('width', "60px").attr('height', "20px").attr('style', "fill:#ededed;stroke:#d7d7d7;stroke-width=:3px");
+    c3.attr('x', 90).attr('y', 299).attr('width', "60px").attr('height', "20px").attr('style', "fill:#ededed;stroke:#d7d7d7;stroke-width=:3px").append('id', "img3");
 
     //var c4 = svgctrl.append('polygon');
 
     //c4.attr('points', "260,10 280,10 290,70").attr('style', "fill:blue;stroke:purple;stroke-width:1");
 
-    var c5 = svgctrl.append('rect');
+    var c4 = svgctrl.append('rect');
 
-    c5.attr('x', "108").attr('y', 28).attr('ry', "10px").attr('width', 40).attr('height', 5).attr('style', 'fill:black;stroke:black;stroke-width:5;opacity:0.5');
+    c4.attr('x', "108").attr('y', 28).attr('ry', "10px").attr('width', 40).attr('height', 5).attr('style', 'fill:black;stroke:black;stroke-width:5;opacity:0.5').append('id', "img4");
 
-    var c6 = svgctrl.append('circle');
+    var c5 = svgctrl.append('circle');
 
-    c6.attr('cx', 175).append('cy', 30).append('r', 5).append('stroke', "black").append('stroke-width', '3').append('fill', 'black');
+    c5.attr('cx', 175).attr('cy', 30).attr('r', 5).attr('stroke', "black").attr('stroke-width', '3').attr('fill', 'black').attr('id', "img5");
+
 
 }
 
-function left() {
-    $('#img1').animate({ left: '200px' }, "slow");
+
+function Strengthen() {
+
+    var svgctrl = d3.select('svg');
+    var c6 = svgctrl.append('rect');
+    c6.attr('x', x1).attr('y', y1 + 33).attr('width', "30px").attr('height', "20px").attr('style', 'fill:black;stroke:black;stroke-width:5;').append('id', "img6");
+    var c7 = svgctrl.append('rect');
+    c7.attr('x', x1 + 10).attr('y', y1 + 53).attr('width', "10px").attr('height', "180px").attr('style', 'fill:black;stroke:black;stroke-width:5;').append('id', "img7");
+    var c8 = svgctrl.append('rect');
+    c8.attr('x', x1 - 10).attr('y', y1 + 223).attr('width', "50px").attr('height', "50px").attr('style', 'fill:black;stroke:black;stroke-width:5;').append('id', "img8");
+    var c9 = svgctrl.append('rect');
+    c9.attr('x', x1).attr('y', y1 + 273).attr('width', "5px").attr('height', "20px").attr('style', 'fill:black;stroke:black;stroke-width:1;').append('id', "img9");
+    var c10 = svgctrl.append('rect');
+    c10.attr('x', x1 + 23).attr('y', y1 + 273).attr('width', "5px").attr('height', "20px").attr('style', 'fill:black;stroke:black;stroke-width:1;').append('id', "img10");
+
 }
-
-
-
-
-//$('#run1').bind('click', Run);
-//$('#run2').bind('click', left);
